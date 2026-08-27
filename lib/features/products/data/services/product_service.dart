d41a6b2e62d202b644b0cbe55ad3484411894b7d
Juan170807 <juanmasanta2007@gmail.com>
2026-08-27 12:36:38 -0500

    fix: update services and project configuration

--- lib/features/products/data/services/product_service.dart
+++ lib/features/products/data/services/product_service.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../../../../core/constants/app_config.dart';
 import '../models/product_model.dart';
 
 class ProductService {
-  static const String baseUrl = 'http://localhost:4000/api/products';
+  static const String baseUrl = '${AppConfig.apiBaseUrl}/products';
   final _storage = const FlutterSecureStorage();
 
   Future<List<ProductModel>> obtenerProductos() async {
-    final token = await _storage.read(key: 'auth_token');
+    final token = await _storage.read(key: 'jwt_token');
 
     final response = await http.get(
       Uri.parse(baseUrl),
